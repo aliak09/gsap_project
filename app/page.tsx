57a1d9b70e3e010dd7440ebde91c68afd99c4c84
aliak09 <aliak9901@gmail.com>
2026-02-24 23:34:27 +0500

    Navbar added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from 'gsap/all';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 export default function page() {
   return (
-    <div className='flex-center h-[100vh]'>
-      <h1 className='flex-center text-4xl text-indigo-300'>Helo GSAP!</h1>
-    </div>
+    <main>
+      <Navbar />
+      <Hero />
+    </main>
   )
 }
