ff2f731ac3c1c733bf198fb5492e9e5b217923a8
aliak09 <aliak9901@gmail.com>
2026-02-26 23:44:10 +0500

    added art section

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Cocktail from './components/Cocktail';
 import About from './components/About';
+import Art from './components/Art';
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 export default function page() {
@@ -13,6 +14,7 @@ export default function page() {
       <Hero />
       <Cocktail />
       <About />
+      <Art/>
       <div className='h-dvh bg-black'></div>
     </main>
   )
